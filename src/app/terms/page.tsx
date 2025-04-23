a9a5b2bbca14375d429e2b1aa54546fa488a23da
Noah Zhao <noahzhao@uw.edu>
2025-04-23 16:59:59 -0700

    Created Terms & Conditions page and styles

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,7 +1,26 @@
+import "../styles/terms.css"
+
 export default function FAQ() {
     return (
-        <div className="min-h-screen bg-white p-8">
-            <h1 className="text-3xl font-bold">Terms & Conditions</h1>
+        <div className="terms-container">
+            <h1 className="terms-title">TERMS & CONDITIONS</h1>
+            <div className="terms-content">
+                <div className="info-box">
+                    <h1 className="terms-header">GENERAL</h1>
+                    <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                         libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                </div>
+                <div className="info-box">
+                    <h1 className="terms-header">ORDERING</h1>
+                    <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                         libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                </div>
+                <div className="info-box">
+                    <h1 className="terms-header">CANCELLATIONS & MODIFICATIONS</h1>
+                    <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+                         libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                </div>
+            </div>
         </div>
     );
 }
