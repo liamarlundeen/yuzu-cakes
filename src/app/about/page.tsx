f96839d2e647c421b3a56e00adee7000edb19bfe
Andy <andyhuynh156@gmail.com>
2025-04-25 14:55:41 -0700

    Add header and paragraph structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,15 @@ import "../styles/about.css"
 export default function About() {
     return (
         <div className="min-h-screen bg-white p-8">
-            <h1 className="text-3xl font-bold">About</h1>
+            <h1 className="text-3xl font-bold">ABOUT</h1>
+            <div>
+                <h1>WHAT IS YUZU CAKE</h1>
+                <p></p>
+            </div>
+            <div>
+                <h1>ABOUT THE OWNER</h1>
+                <p></p>
+            </div>
         </div>
     );
 }
