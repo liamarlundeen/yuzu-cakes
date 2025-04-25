d0aec80d7485d3575e106002d77c00ababa2e442
Andy <andyhuynh156@gmail.com>
2025-04-25 15:10:02 -0700

    Add about title styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import "../styles/about.css"
 export default function About() {
     return (
         <div className="min-h-screen bg-white p-8">
-            <h1 className="text-3xl font-bold">ABOUT</h1>
+            <h1 className="about-title">ABOUT</h1>
             <div>
                 <h1>WHAT IS YUZU CAKE</h1>
                 <p></p>
